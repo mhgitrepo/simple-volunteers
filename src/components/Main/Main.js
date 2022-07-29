d9eadf809bec4223088a0c38fd88f11f7ad3ae11
Mehedi Hasan <13490077+mhgitrepo@users.noreply.github.com>
2022-07-30 03:36:53 +0600

    implementation of search in a good way

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css';
 
 const Main = () => {
-    const [volunteers] = useVolunteers([]);
+    // const [volunteers] = useVolunteers([]);
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect(() => {
+        console.log('inside use Effect.');
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => {
+            const match = data.filter(d => d.title.toLowerCase().includes(searchText));
+            setSearchResult(match);
+        })
+    }, [searchText])
+
     const handleSearchChange = event => {
+        setSearchText(event.target.value);
+    }
+
+    /* const handleSearchChange = event => {
         const searchText = event.target.value;
         const match = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
         setSearchResult(match);
-    }
+    } */
 
     return (
         <div>
-            <h2>Volunteer Activities: {volunteers.length}</h2>
+            <h2>Volunteer Activities: {searchResult.length}</h2>
             <div style={{'margin' : '20px'}}>
                 <input onChange={handleSearchChange} type="text" placeholder='search'/>
             </div>
